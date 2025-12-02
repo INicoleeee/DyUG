37fc0d016305bcab618c7b54ae83e0ab9d8e1bcb
INicoleeee <438495619@qq.com>
2025-12-02 22:33:39 +0800

    依赖注入

--- app/src/main/java/com/example/dydemo/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/dydemo/data/local/database/AppDatabase.kt
@@ -1,15 +1,14 @@
 package com.example.dydemo.data.local.database
 
-/**
- * Room数据库配置
- */
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.dydemo.data.local.entity.MessageEntity
 import com.example.dydemo.data.local.entity.UserEntity
 
-// 数据库版本号，如果表结构变化需要升级
-@Database(entities = [UserEntity::class], version = 4, exportSchema = false)
+@Database(entities = [UserEntity::class, MessageEntity::class], version = 5, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-    // 暴露 DAO 接口
+
     abstract fun userDao(): UserDao
+    
+    abstract fun messageDao(): MessageDao
 }
